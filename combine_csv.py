d40ab110bd16b7affff15976bb993217fa5187f1
Adam Dill <adamtdill@gmail.com>
2021-08-25 07:26:40 -0500

    Breaking logic into functions.

--- combine_csv.py
+++ combine_csv.py
@@ -3,39 +3,26 @@ import os
 import sys, getopt
 import csv
 
-def main(argv):
-	# handle the arguments
-	directory = os.getcwd() + "/"
-	out_file = "output.csv"
-	try:
-		opts, args = getopt.getopt(argv,"hd:o:",["dir=", "out="])
-	except getopt.GetoptError:
-		print('combine_csv.py -d <directory> -o <output>')
-		sys.exit(2)
-	for opt, arg in opts:
-		if opt == '-h':
-			print('combine_csv.py -d <directory> -o <output>')
-			sys.exit()
-		elif opt in ("-d", "--dir"):
-			directory = arg
-		elif opt in ("-o", "--out"):
-			out_file = arg
-	
-	# the mapping of header names
-	map = dict({
-		'firstName': ['First Name', 'fname'],
-		'lastName': ['Last Name', 'lname'],
-		'email': ['Email', 'email'],
-		'ssn': ['SSN', 'social']
-	})
-	
-	
+
+# the mapping of header names
+map = dict({
+	'firstName': ['First Name', 'fname'],
+	'lastName': ['Last Name', 'lname'],
+	'email': ['Email', 'email'],
+	'ssn': ['SSN', 'social']
+})
+
+
+def getColumns():
 	out_columns = []
 	for key in map:
 		out_columns.append(key)
-		
+	return out_columns
+
+def processFiles(directory):
 	out_data = []
 	files = os.listdir(directory)
+	print(directory)
 	for file in files:
 		if file.endswith(".csv"):
 			with open(directory + file) as csvfile:
@@ -50,7 +37,10 @@ def main(argv):
 								row_data[key] = v
 					if bool(row_data):
 						out_data.append(row_data)
-		
+	return out_data
+
+def writeFile(out_file, out_data):
+	out_columns = getColumns()
 	try:
 		with open(out_file, 'w') as csvfile:
 			writer = csv.DictWriter(csvfile, fieldnames=out_columns)
@@ -59,6 +49,32 @@ def main(argv):
 				writer.writerow(data)
 	except IOError:
 		print("I/O error")
+
+def main(argv):
+	# the directory to process (optional argument)
+	directory = os.getcwd() + "/"
+	
+	# the output file (optional argument)
+	out_file = "output.csv"
+	
+	# handle the arguments
+	try:
+		opts, args = getopt.getopt(argv,"hd:o:",["dir=", "out="])
+	except getopt.GetoptError:
+		print('combine_csv.py -d <directory> -o <output>')
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print('combine_csv.py -d <directory> -o <output>')
+			sys.exit()
+		elif opt in ("-d", "--dir"):
+			directory = arg
+		elif opt in ("-o", "--out"):
+			out_file = arg
+			
+	out_columns = getColumns()
+	out_data = processFiles(directory)
+	writeFile(out_file, out_data)
 	
 
 if __name__ == "__main__":
